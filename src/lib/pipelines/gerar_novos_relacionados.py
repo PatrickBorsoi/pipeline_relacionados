1ec86490ab1fd197b3a3b46a9826a459f67636e9
Patrick Borsoi <patrickperez1698@gmail.com>
2025-01-10 18:00:53 -0300

    adicionando o arquivo para gerar novos relacionados

---
+++ src/lib/pipelines/gerar_novos_relacionados.py
@@ -0,0 +1,11 @@
+import xmltodict
+import pandas as pd
+from datetime import datetime, timedelta
+import re
+from dotenv import load_dotenv
+
+from lib.classes.connection_api_google import *
+
+
+load_dotenv('.env')
+
